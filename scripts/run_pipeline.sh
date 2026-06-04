7f0fdcc11cf701613cb2de54e5f74ce1f2a24620
Microbial Evolution UoN <157133461+Alhubb@users.noreply.github.com>
2026-06-04 11:30:25 +0100

    Change file pattern to match .fasta files

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -79,7 +79,7 @@ for dir in "$EXTRACTED"/*; do
     fi
 
     shopt -s nullglob
-    files=( "$dir"/*.bakta."$gene".fasta )
+    files=( "$dir"/*.fasta )
     shopt -u nullglob
 
     if [ ${#files[@]} -eq 0 ]; then
